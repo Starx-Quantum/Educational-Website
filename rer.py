b159bab112c8fa85ce40883d3bd2e362d9bfb3d7
Aman Pandey(Lucifer) <113250329+Starx-Quantum@users.noreply.github.com>
2022-11-07 13:10:23 +0530

    Add files via upload

---
+++ rer.py
@@ -0,0 +1,22 @@
+#s=input('data: ')
+#l=s.split(',')
+#print('list',1)
+#res=[]
+#for i in 1:
+ #   if i not in res:
+  #      res.append()
+#print('After Deleting Duplicates', res)        
+#s=input('data: ')
+#l=list(map(int,s.split(',')))
+#print('list',1)
+#s=0
+#for i in 1:
+#
+#print(s) 
+l=[]
+for i in range(2000, 3201):
+    if i %7==0 and i %5 !=0:
+        l.append(i)
+        
+print(l)
+   
